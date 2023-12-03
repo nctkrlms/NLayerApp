2f0cc2072856837d79440d03e838a2f07b099052
nctkrlms <75181243+nctkrlms@users.noreply.github.com>
2023-12-03 12:56:48 +0300

    Add methods to Generic Repository at Ropsitory Layer.

--- NLayer.Core/Repostories/IGenericRepository.cs
+++ NLayer.Core/Repostories/IGenericRepository.cs
@@ -10,6 +10,7 @@ namespace NLayer.Core.Repostories
     public interface IGenericRepository<T> where T : class
     {
         Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
         Task AddAsync(T entity);
