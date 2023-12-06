22348fd208d5a9f424fe0a84ef24f679480b3e36
nctkrlms <75181243+nctkrlms@users.noreply.github.com>
2023-12-06 03:22:50 +0300

    Modified program.cs for mapping and services.

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -22,8 +22,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped(typeof(IService<>),typeof(Services<>));
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 builder.Services.AddAutoMapper(typeof(MapProfile));
 builder.Services.AddDbContext<AppDbContext>(x =>
